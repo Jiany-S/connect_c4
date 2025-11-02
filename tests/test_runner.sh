d5f29622c7203c8b2a072dca60d31224120a66ab
JadMouawad <129901327+JadMouawad@users.noreply.github.com>
2025-11-02 12:36:50 +0200

    Add test_bot_medium to test runner script

--- tests/test_runner.sh
+++ tests/test_runner.sh
@@ -5,9 +5,11 @@ mkdir -p build
 echo "🔹 Building unit tests..."
 gcc -Wall -Wextra -std=gnu11 -g -o build/test_board tests/test_board.c src/board.c
 gcc -Wall -Wextra -std=gnu11 -g -o build/test_game tests/test_game.c src/board.c src/game.c
+gcc -Wall -Wextra -std=gnu11 -g -o build/test_bot_medium tests/test_bot_medium.c src/board.c src/bot_medium.c src/game.c
 
 echo "🔹 Running unit tests..."
 ./build/test_board
 ./build/test_game
+./build/test_bot_medium
 
 echo "✅ All unit tests passed."
