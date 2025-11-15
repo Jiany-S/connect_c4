a6542731d487973f2cab450659f23a8a62b69e76
JadMouawad <129901327+JadMouawad@users.noreply.github.com>
2025-11-15 21:07:27 +0200

    Modify bot_hard.h for game integration and threading
    Updated header file to include 'game.h' and added multithreading support.

--- src/bot_hard.h
+++ src/bot_hard.h
@@ -1,8 +1,25 @@
+// src/bot_hard.h
 #ifndef BOT_HARD_H
 #define BOT_HARD_H
 
-#include "board.h"
+#include "game.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Toggle multithreading: 1 = use threads, 0 = single-threaded
+#define USE_THREADS 1
+
+#ifdef USE_THREADS
+#define MT_BUILD
+#endif
+
+// Main function to get Hard Bot move
 int getHardMove(char board[ROWS][COLS], char player);
 
+#ifdef __cplusplus
+}
 #endif
+
+#endif // BOT_HARD_H
